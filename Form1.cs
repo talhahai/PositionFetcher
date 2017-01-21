8b1d8b17a6d917836ee87e8d178804be126b4bf3
Syed Talha Hai <talhahai@yahoo.com>
2017-01-21 19:12:13 +0500

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PositionFetcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = button1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Cursor.Position.X.ToString();
+            textBox2.Text = Cursor.Position.Y.ToString();
+        }
+    }
+}
